73cce27b3029c72b78eff8c4409302b417fa799b
FreeIce88 <283052623@qq.com>
2016-03-06 00:13:19 +0800

    Grunt-demo
    Grunt-demo

---
+++ grunt_test/Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt){
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		uglify: {
+			options: {
+				stripBanners: true,
+				banner: '/*! <%= pkg.name %>-<%-pkg.version%>.js <%= grunt.template.today("yyyy-mm-dd") %> */\n'//添加banner
+			},
+			build: {
+				src: 'src/test.js',
+				dest: 'build/<%= pkg.name %>-<%-pkg.version%>.js.min.js'
+			}
+		},
+
+		jshint: {
+			build: [ 'Gruntfile.js', 'src/*.js' ],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
+
+		watch: {
+			build: {
+				files: ['src/*.js' , 'src/*.css'],
+				tasks: ['jshint', 'uglify'],
+				options: { spawn: false}
+			}
+		}
+	});
+
+	
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['jshint', 'uglify','watch']);
+};
---
+++ grunt_test/src/test.js
@@ -0,0 +1,14 @@
+(function(window,undefined){
+
+	function add(a,b){
+		return a + b;
+	}
+
+	add(10,100);
+})(window);
+
+
+function del(c,d){
+	return c - d;
+}
+del(11,2);
